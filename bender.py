d54c0e1ad3fd3a64f68d6a44822c207d439a38d8
Ne00n <neo@tiefkuehler-madness.me>
2025-12-03 21:52:08 +0100

    rename to data

--- bender.py
+++ bender.py
@@ -74,19 +74,19 @@ for asn in toLoad:
                 for prefix,subnets in asnData.items():
                     if "::" in prefix: continue
                     settings = {}
-                    for entry, latency in subnets.items():
+                    for entry, data in subnets.items():
                         if "ignoreSubnets" in config and entry in config['ignoreSubnets']: continue
-                        if entry == "settings": settings = latency
-                        if not "/" in entry or not latency: continue
+                        if entry == "settings": settings = data
+                        if not "/" in entry or not data: continue
                         if not asn in routing: routing[asn] = {}
                         if settings and 'any' in settings:
-                            for entry in latency:
+                            for entry in data:
                                 entry, avrg = f"{entry[0]}/32", float(entry[1])
-                                if not entry in routing[asn]: routing[asn][entry] = {"latency":999,"region":None,"asn":None}
+                                if not entry in routing[asn]: routing[asn][entry] = {"data":999,"region":None,"asn":None}
                                 if routing[asn][entry]['latency'] > avrg:
                                     routing[asn][entry] = {"latency":avrg,"location":location,"asn":asn}
                         else:
-                            avrg = tools.getAvrg(latency)
+                            avrg = tools.getAvrg(data)
                             if not entry in routing[asn]: routing[asn][entry] = {"latency":999,"region":None,"asn":None}
                             if routing[asn][entry]['latency'] > avrg:
                                 routing[asn][entry] = {"latency":avrg,"location":location,"asn":asn}
